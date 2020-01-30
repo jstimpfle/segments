5ed8ec65e77de80d2787693d1e10c9e8b3693a09
Jens Stimpfle <jfs@jstimpfle.de>
2020-01-30 23:03:01 +0100

    update

--- src/segments/main.c
+++ src/segments/main.c
@@ -1,14 +1,10 @@
 #include <segments/opengl.h>
 #include <segments/gfx.h>
 
-#define LENGTH(a) (sizeof (a) / sizeof (a)[0])
-
 int main(void)
 {
         create_opengl_context();
         setup_opengl();
-
         do_gfx();
-
         return 0;
 }
